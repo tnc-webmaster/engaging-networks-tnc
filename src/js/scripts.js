3b713e30459f603050701b60511a41956de910ac
Phil Winston <phillip.winston@tnc.org>
2022-04-08 17:05:19 -0400

    add front end validation for "other amount" field

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -856,6 +856,8 @@
     const feeCoverCheckbox = theForm.querySelector('[name="transaction.feeCover"]');
     const giftDesignationYN = theForm.querySelector('.en__field--gift-designation-yn');
     const otherAmountInput = theForm.querySelector(otherAmountInputSelector);
+    const otherAmountInputMin = theForm.querySelector('.en__otherFieldMin input');
+    const otherAmountErrorContainer = document.createElement('<div class="en__other__field__error"></div>');
     const recurrenceCheckbox = theForm.querySelector('[name="transaction.recurrpay"][type="checkbox"]');
     const recurrenceFrequency = theForm.querySelector('.en__field--recurrfreq');
     let donationAmtRadios = null;
@@ -870,9 +872,31 @@
       }
     };
 
+    const otherFieldClear = () => {
+      otherAmountInput.classList.remove('_checkAmtErr');
+      theForm.querySelector('.en__other__field__error').textContent('');
+      otherAmountInputMin.value = '';
+      theForm.querySelector('.en__submit button').disabled = false;
+    };
+
     const handleDonationAmountChange = (e) => {
       if (feeCoverCheckbox) {
         maybeUncheckFeeCover(getOriginalDonationAmount());
+      }
+    };
+
+    const validateDonationAmountChangeMin = (e) => {
+      otherAmountInput.append(otherAmountErrorContainer);
+      if (otherAmountInputMin && otherAmountInputMin != null) {
+        if (!e.target.value || e.target.value >= otherAmountInputMin.value) {
+          e.target.classList.remove('_checkAmtErr');
+          theForm.querySelector('.en__other__field__error').textContent('');
+          theForm.querySelector('.en__submit button').disabled = false;
+        } else if (e.target.value < otherAmountInputMin.value) {
+          e.target.classList.add('_checkAmtErr');
+          theForm.querySelector('.en__other__field__error').textContent('Your donation must be between $'+otherAmountInputMin.value+'.00 and $50,000.00');
+          theForm.querySelector('.en__submit button').disabled = true;
+        }
       }
     };
 
@@ -893,7 +917,16 @@
       // Listen for other amount change
       if (otherAmountInput) {
         otherAmountInput.addEventListener('input', handleDonationAmountChange);
+        otherAmountInput.addEventListener('focusout', validateDonationAmountChangeMin);
+
+        document.addEventListener('click', function(event) {
+          if (event.target.matches('label.en__field__label[for*="transaction_donationAmt"]')) {
+            event.preventDefault();
+            otherFieldClear();
+          }
+        }, false);
       }
+
     };
 
     resetDonationAmount = () => {
